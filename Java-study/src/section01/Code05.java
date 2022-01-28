f3cb77889de8c2fa62dd1b4b5780e6d8d15e45d7
tomitahajime <ponponpon816@gmail.com>
2022-01-29 00:11:10 +0900

    [Add]p23 第0章Javaをはじめよう,0.2_はじめてのプログラミング,0.2.5_計算させてみよう,0-5_計算をする

---
+++ Java-study/src/section01/Code05.java
@@ -0,0 +1,16 @@
+// p23 第0章Javaをはじめよう,0.2_はじめてのプログラミング,0.2.5_計算させてみよう
+// 0-5_計算をする
+
+package section01;
+
+public class Code05 {
+	public static void main(String[] args) {
+		System.out.println("すがわら");
+		System.out.println("31歳です");
+		System.out.println("お酒が好きです");
+		System.out.println("31 + 31の計算をします");
+		// 数式
+		System.out.println(31 + 31);
+		// 実行結果 62
+	}
+}
