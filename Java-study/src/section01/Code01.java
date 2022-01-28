fb0fd954cd6232aa2a60e09e5952899346c238d7
tomitahajime <ponponpon816@gmail.com>
2022-01-28 23:42:47 +0900

    [First]p20 第0章Javaをはじめよう,0.2_はじめてのプログラミング,0.2.2_サンプルプログラムを動かしてみよう,0-1_HelloWorldプログラム

---
+++ Java-study/src/section01/Code01.java
@@ -0,0 +1,10 @@
+// p20 第0章Javaをはじめよう,0.2_はじめてのプログラミング,0.2.2_サンプルプログラムを動かしてみよう
+// 0-1_HelloWorldプログラム
+
+package section01;
+
+public class Code01 {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+}
