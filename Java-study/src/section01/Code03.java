dc862d5008c410e08ea906d9be6690b68dd38f64
tomitahajime <ponponpon816@gmail.com>
2022-01-29 00:00:33 +0900

    [Add]p21 第0章Javaをはじめよう,0.2_はじめてのプログラミング,0.2.3_画面に好きな文字を表示させよう,0-3_湊くんが作ったプログラム（エラー）

---
+++ Java-study/src/section01/Code03.java
@@ -0,0 +1,11 @@
+// p21 第0章Javaをはじめよう,0.2_はじめてのプログラミング,0.2.3_画面に好きな文字を表示させよう
+// 0-3_湊くんが作ったプログラム（エラー）
+
+package section01;
+
+public class Code03 {
+	public static void main(String[] args) {
+		// "記号がないためエラーが起きる
+		// System.out.println("湊君かっこいい！最高！);
+	}
+}
