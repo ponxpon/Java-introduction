14635690fb5b28dbaa0d3c1cde823ab67435a401
tomitahajime <ponponpon816@gmail.com>
2022-01-28 23:55:50 +0900

    [Add]p21 第0章Javaをはじめよう,0.2_はじめてのプログラミング,0.2.3_画面に好きな文字を表示させよう,0-2_一部を置き換えたプログラム

---
+++ Java-study/src/section01/Code02.java
@@ -0,0 +1,10 @@
+// p21 第0章Javaをはじめよう,0.2_はじめてのプログラミング,0.2.3_画面に好きな文字を表示させよう
+// 0-2_一部を置き換えたプログラム
+
+package section01;
+
+public class Code02 {
+	public static void main(String[] args) {
+		System.out.println("Java SE 11 Silver");
+	}
+}
