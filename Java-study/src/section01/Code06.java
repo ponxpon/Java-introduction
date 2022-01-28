a940ec96a03304a397027a42df5f72547c866537
tomitahajime <ponponpon816@gmail.com>
2022-01-29 00:21:55 +0900

    [Add]p24 第0章Javaをはじめよう,0.2_はじめてのプログラミング,0.2.6_変数を使ってみよう,0-6_変数を使ってみる

---
+++ Java-study/src/section01/Code06.java
@@ -0,0 +1,20 @@
+// p24 第0章Javaをはじめよう,0.2_はじめてのプログラミング,0.2.6_変数を使ってみよう
+// 0-6_変数を使ってみる
+
+package section01;
+
+public class Code06 {
+	public static void main(String[] args) {
+		System.out.println("すがわら");
+		System.out.println("31歳です");
+		System.out.println("お酒が好きです");
+		System.out.println("31 + 31の計算をします");
+		System.out.println(31 + 31);
+		// 変数xを準備する
+		int x;
+		// xに6を入れる
+		x = 6;
+		System.out.println(x * x * 3.14);
+		// 変数を使った計算結果 113.04
+	}
+}
