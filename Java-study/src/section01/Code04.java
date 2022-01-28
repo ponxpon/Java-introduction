c68964a0272f715ed0027295cd91c1a38fbe566e
tomitahajime <ponponpon816@gmail.com>
2022-01-29 00:07:07 +0900

    [Add]p22 第0章Javaをはじめよう,0.2_はじめてのプログラミング,0.2.4_たくさんの文章を表示しよう,0-4_複数の文章を表示する

---
+++ Java-study/src/section01/Code04.java
@@ -0,0 +1,12 @@
+// p22 第0章Javaをはじめよう,0.2_はじめてのプログラミング,0.2.4_たくさんの文章を表示しよう
+// 0-4_複数の文章を表示する
+
+package section01;
+
+public class Code04 {
+	public static void main(String[] args) {
+		System.out.println("すがわら");
+		System.out.println("31歳です");
+		System.out.println("お酒が好きです");
+	}
+}
