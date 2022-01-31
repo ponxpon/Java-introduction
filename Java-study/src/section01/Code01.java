848a278084a482a11a4278572a6987f148bc948b
tomitahajime <ponponpon816@gmail.com>
2022-01-31 22:43:05 +0900

    [Add]p36 第1章プログラムの書き方,1.2_Javaプログラムの基本構造,1.2.1_プログラムの骨格,1-1_ソースコードの基本構造

---
+++ Java-study/src/section01/Code01.java
@@ -0,0 +1,14 @@
+// p36 第1章プログラムの書き方,1.2_Javaプログラムの基本構造,1.2.1_プログラムの骨格
+// 1-1_ソースコードの基本構造
+
+package section01;
+
+public class Code01 {
+	public static void main(String[] args) {
+		// 処理・命令の部分
+		System.out.println("RPG:スッキリ魔王征伐");
+		System.out.println("Ver.0.1 by 湊");
+		System.out.println("＜ただいま鋭意学習・製作中＞");
+		System.out.println("プログラムを終了する");
+	}
+}
