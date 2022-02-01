615a4b27d3b70663e8d76c706356fddb97af2575
tomitahajime <ponponpon816@gmail.com>
2022-02-01 23:25:09 +0900

    [Add]p46 第1章プログラムの書き方,1.3_変数宣言の文,1.3.1_変数宣言の文とは？,1-2_変数宣言の文

---
+++ Java-study/src/section01/Code02.java
@@ -0,0 +1,15 @@
+// p46 第1章プログラムの書き方,1.3_変数宣言の文,1.3.1_変数宣言の文とは？
+// 1-2_変数宣言の文
+
+package section01;
+
+public class Code02 {
+	public static void main(String[] args) {
+		// 変数宣言の文（ageという箱を用意）
+		int age;
+		// 箱に数字の「30」を入れる
+		age = 30;
+		// 箱の中身を表示
+		System.out.println(age);
+	}
+}
