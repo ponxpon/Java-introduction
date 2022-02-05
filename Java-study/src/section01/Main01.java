2353c6f1640adef9b70585280d62ec7343af4370
tomitahajime <ponponpon816@gmail.com>
2022-02-05 23:01:30 +0900

    [Add]p58 第1章プログラムの書き方,1.5_練習問題,練習1-2

---
+++ Java-study/src/section01/Main01.java
@@ -0,0 +1,14 @@
+// p58 第1章プログラムの書き方,1.5_練習問題
+// 練習1-2
+
+package section01;
+
+// 次の結果を表示するソースコードを作成
+// 縦幅3横幅5の長方形の面積は、15
+public class Main01 {
+	public static void main(String[] args) {
+		int a = 3; int b = 5;
+		int c = a * b;
+		System.out.println("縦幅" + a + "横幅" + b + "の長方形の面積は、" + c);
+	}
+}
