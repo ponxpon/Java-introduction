92521060c41ef146119a7905b093decd933208bf
tomitahajime <ponponpon816@gmail.com>
2022-02-06 23:51:44 +0900

    [Add]p58 第1章プログラムの書き方,1.5_練習問題,練習1-3

---
+++ Java-study/src/section01/Main02.java
@@ -0,0 +1,12 @@
+// p58 第1章プログラムの書き方,1.5_練習問題
+// 練習1-3
+
+package section01;
+
+public class Main02 {
+	boolean result = true;
+	char favoriteCharacter = '駆';
+	double pi = 3.14;
+	long number = 314159265853979L;
+	String msg = "ミナトの攻撃！的に15ポイントのダメージを与えた。";
+}
