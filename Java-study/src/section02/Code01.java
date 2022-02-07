1bc52c8705b55876f63389caac3c1c0b02a3c196
tomitahajime <ponponpon816@gmail.com>
2022-02-07 23:43:44 +0900

    [Add]p62 第2章式と演算子,2.1_計算の文,2.1.1_計算の文とは？,2-1_変数宣言の文と計算の文

---
+++ Java-study/src/section02/Code01.java
@@ -0,0 +1,18 @@
+// p62 第2章式と演算子,2.1_計算の文,2.1.1_計算の文とは？
+// 2-1_変数宣言の文と計算の文
+
+package section02;
+
+public class Code01 {
+	public static void main(String[] args) {
+		// 変数宣言の文
+		int a;
+		int b;
+		// 計算の文（代入）
+		a = 20;
+		// 計算の文（足し算して代入）
+		b = a + 5;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
