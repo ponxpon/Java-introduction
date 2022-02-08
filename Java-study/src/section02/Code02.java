bf9e708a27babb029f1ada98225ab8125d82b413
tomitahajime <ponponpon816@gmail.com>
2022-02-08 23:43:05 +0900

    [Add]p66 第2章式と演算子,2.2_オペランド,2.2.2_エスケープシーケンス,2-2 エスケープシーケンスを用いていない例（エラー）

---
+++ Java-study/src/section02/Code02.java
@@ -0,0 +1,11 @@
+// p66 第2章式と演算子,2.2_オペランド,2.2.2_エスケープシーケンス
+// 2-2 エスケープシーケンスを用いていない例（エラー）
+
+package section02;
+
+public class Code02 {
+	public static void main(String[] args) {
+		// 一部の部分だけが文字列と見なされる
+		// System.out.println("私の好きな記号は二重引用符 （"） です");
+	}
+}
