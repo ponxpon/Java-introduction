8d17a0314e92a43cf8b7d734aec013181a429a9d
tomitahajime <ponponpon816@gmail.com>
2022-02-09 23:46:51 +0900

    [Add]p67 第2章式と演算子,2.2_オペランド,2.2.2_エスケープシーケンス,2-3 エスケープシーケンスを用いた例

---
+++ Java-study/src/section02/Code03.java
@@ -0,0 +1,11 @@
+// p67 第2章式と演算子,2.2_オペランド,2.2.2_エスケープシーケンス
+// 2-3 エスケープシーケンスを用いた例
+
+package section02;
+
+public class Code03 {
+	public static void main(String[] args) {
+		// \"によって二重引用符は文字と見なされる
+		System.out.println("私の好きな記号は二重引用符 （\"） です");
+	}
+}
