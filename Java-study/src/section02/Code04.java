dac3db4082626b58a80a1e0de918d286c79b56a9
tomitahajime <ponponpon816@gmail.com>
2022-02-10 22:44:13 +0900

    [Add]p74 第2章式と演算子,2.4_演算子,2.4.4_インクリメント/デクリメント演算子,2-4_インクリメント演算子

---
+++ Java-study/src/section02/Code04.java
@@ -0,0 +1,14 @@
+// p74 第2章式と演算子,2.4_演算子,2.4.4_インクリメント/デクリメント演算子
+// 2-4_インクリメント演算子
+
+package section02;
+
+public class Code04 {
+	public static void main(String[] args) {
+		int a;
+		a = 100;
+		// aの内容が1増える
+		a++;
+		System.out.println(a);
+	}
+}
