f30c4949c0babc3061daf3deecc9c5601bdcc42e
tomitahajime <ponponpon816@gmail.com>
2022-02-15 23:51:58 +0900

    [Add]p80 第2章式と演算子,2.5_型の変換,2.5.2_代入時の自動型変換,2-6_データより小さな型の変数に代入（エラー）

---
+++ Java-study/src/section02/Code06.java
@@ -0,0 +1,11 @@
+// p80 第2章式と演算子,2.5_型の変換,2.5.2_代入時の自動型変換
+// 2-6_データより小さな型の変数に代入（エラー）
+
+package section02;
+
+public class Code06 {
+	public static void main(String[] args) {
+		// 大きな型の値を小さな型の変数に代入することは原則できない。
+		// int i = 3.2;
+	}
+}
