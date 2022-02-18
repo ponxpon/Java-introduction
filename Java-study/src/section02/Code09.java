c5dfdf1bd94437e35b5d7cd4ecfa0401838b88f8
tomitahajime <ponponpon816@gmail.com>
2022-02-18 23:11:55 +0900

    [Add]p85 第2章式と演算子,2.5_型の変換,2.5.4_演算時の自動型変換,2-9_文字列の連結

---
+++ Java-study/src/section02/Code09.java
@@ -0,0 +1,11 @@
+// p85 第2章式と演算子,2.5_型の変換,2.5.4_演算時の自動型変換
+// 2-9_文字列の連結
+
+package section02;
+
+public class Code09 {
+	public static void main(String[] args) {
+		String msg = "私の年齢は" + 23;
+		System.out.println(msg);
+	}
+}
