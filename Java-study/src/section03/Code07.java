12e392557d06c5950d21a6d563b91351f49da41e
tomitahajime <ponponpon816@gmail.com>
2022-03-06 06:05:04 +0900

    [Add]p126 第3章条件分岐と繰り返し,3.5_繰り返し構文のバリエーション,3.5.2_for文による繰り返し,3-7_基本的なfor文

---
+++ Java-study/src/section03/Code07.java
@@ -0,0 +1,13 @@
+// p126 第3章条件分岐と繰り返し,3.5_繰り返し構文のバリエーション,3.5.2_for文による繰り返し
+// 3-7_基本的なfor文
+
+package section03;
+
+public class Code07 {
+	public static void main(String[] args) {
+		// for文を利用して「こんにちは」の表示を10回繰り返す
+		for (int i = 0; i < 10; i++) {
+			System.out.println("こんにちは");
+		}
+	}
+}
