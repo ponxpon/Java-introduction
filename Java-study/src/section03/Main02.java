95e02152ddfd22e0e1b21ca37b684e454efea10c
tomitahajime <ponponpon816@gmail.com>
2022-03-12 00:01:40 +0900

    p136 第3章条件分岐と繰り返し,3.8_練習問題,練習3-5_switch文の作成

---
+++ Java-study/src/section03/Main02.java
@@ -0,0 +1,17 @@
+// p136 第3章条件分岐と繰り返し,3.8_練習問題
+// 練習3-4_コード修正
+
+package section03;
+
+public class Main02 {
+	public static void main(String[] args) {
+		boolean tenki = true;
+		if (tenki == false) {
+			System.out.println("洗濯をします");
+			System.out.println("散歩に行きます");
+		} else {
+			System.out.println("DVDを見ます");
+			System.out.println("寝ます");
+		}
+	}
+}
