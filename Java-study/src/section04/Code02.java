fe74c1777b9e1d154fe743dd9f66613694450d44
tomitahajime <ponponpon816@gmail.com>
2022-03-14 23:23:05 +0900

    [Add]p150 第4章配列,4.2_配列の書き方,4.2.1_配列の作成,4-2_配列の作成手順

---
+++ Java-study/src/section04/Code02.java
@@ -0,0 +1,13 @@
+// p150 第4章配列,4.2_配列の書き方,4.2.1_配列の作成
+// 4-2_配列の作成手順
+
+package section04;
+
+public class Code02 {
+	public static void main(String[] args) {
+		// int型の要素を代入できる配列変数scoresを用意（[]が必要）
+		int[] scores;
+		// int型の要素を5つ作成してscoresに代入し、配列scoresの完成
+		scores = new int[5];
+	}
+}
