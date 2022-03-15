0510e80ba30288cea1cb343b4a28bd60e5abcf1f
tomitahajime <ponponpon816@gmail.com>
2022-03-15 23:51:23 +0900

    p150 第4章配列,4.2_配列の書き方,4.2.1_配列の作成,4-3_配列変数の宣言と要素の作成と代入を同時に行う

---
+++ Java-study/src/section04/Code03.java
@@ -0,0 +1,10 @@
+// p150 第4章配列,4.2_配列の書き方,4.2.1_配列の作成
+// 4-3_配列変数の宣言と要素の作成と代入を同時に行う
+
+package section04;
+
+public class Code03 {
+	public static void main(String[] args) {
+		int[] scores = new int[5];
+	}
+}
