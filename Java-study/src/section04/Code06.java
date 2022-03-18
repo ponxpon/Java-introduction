102bb41c9ccb02eb385413f84346ad976c739654
tomitahajime <ponponpon816@gmail.com>
2022-03-18 23:07:43 +0900

    [Add]p152 第4章配列,4.2_配列の書き方,4.2.3_配列の初期化,4-6_初期化されていない変数を利用（エラー）

---
+++ Java-study/src/section04/Code06.java
@@ -0,0 +1,12 @@
+// p152 第4章配列,4.2_配列の書き方,4.2.3_配列の初期化
+// 4-6_初期化されていない変数を利用（エラー）
+
+package section04;
+
+public class Code06 {
+	public static void main(String[] args) {
+		int x;
+		// xが初期化されていないので、コンパイルエラーになる。
+		// System.out.println(x);
+	}
+}
