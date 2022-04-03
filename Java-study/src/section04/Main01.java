bdf5546ed4ccb3c3f1a9d2e4c2e32f8e23dce3a0
tomitahajime <ponponpon816@gmail.com>
2022-04-03 23:44:43 +0900

    [Add]p176 第4章配列,4.9_練習問題,練習4-1_配列を準備するプログラム

---
+++ Java-study/src/section04/Main01.java
@@ -0,0 +1,15 @@
+// p176 第4章配列,4.9_練習問題
+// 練習4-1_配列を準備するプログラム
+
+package section04;
+
+public class Main01 {
+	// int型の値を4つまとめて格納できる配列points
+	int[] points = new int[4];
+	// double型の値を5つまとめて格納できる配列weights
+	double[] weights = new double[5];
+	// boolean型の値を3つまとめて格納できる配列answers
+	boolean[] answers = new boolean[3];
+	// String型の値を3つまとめて格納できる配列names
+	String[] names = new String[3];
+}
