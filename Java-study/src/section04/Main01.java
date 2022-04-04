a4296df122926b4102dfe990577b2d07048f645f
tomitahajime <ponponpon816@gmail.com>
2022-04-04 23:26:39 +0900

    [Fix]p176 第4章配列,4.9_練習問題,練習4-1_配列を準備するプログラム

--- Java-study/src/section04/Main01.java
+++ Java-study/src/section04/Main01.java
@@ -4,12 +4,14 @@
 package section04;
 
 public class Main01 {
-	// int型の値を4つまとめて格納できる配列points
-	int[] points = new int[4];
-	// double型の値を5つまとめて格納できる配列weights
-	double[] weights = new double[5];
-	// boolean型の値を3つまとめて格納できる配列answers
-	boolean[] answers = new boolean[3];
-	// String型の値を3つまとめて格納できる配列names
-	String[] names = new String[3];
+	public static void main(String[] args) {
+		// int型の値を4つまとめて格納できる配列points
+		int[] points = new int[4];
+		// double型の値を5つまとめて格納できる配列weights
+		double[] weights = new double[5];
+		// boolean型の値を3つまとめて格納できる配列answers
+		boolean[] answers = new boolean[3];
+		// String型の値を3つまとめて格納できる配列names
+		String[] names = new String[3];
+	}
 }
