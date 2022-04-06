1ca290be9f438fdf6fa77e9e2995c79d2555044e
tomitahajime <ponponpon816@gmail.com>
2022-04-06 23:26:32 +0900

    [Add]p184 第5章メソッド,5.1_メソッドを利用するメリット,5.1.2_メソッドの定義,5-1_シンプルなメソッドの定義

---
+++ Java-study/src/section05/Code01.java
@@ -0,0 +1,12 @@
+// p184 第5章メソッド,5.1_メソッドを利用するメリット,5.1.2_メソッドの定義
+// 5-1_シンプルなメソッドの定義
+
+package section05;
+
+public class Code01 {
+	// helloメソッドの定義
+	public static void hello() {
+		// helloメソッドが呼び出された時の処理
+		System.out.println("湊さん、こんにちは");
+	}
+}
